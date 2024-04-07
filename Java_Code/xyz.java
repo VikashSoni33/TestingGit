4e695a5cebe98ac17166ca7636fbfcd52495308b
VikashSoni33 <166296297+VikashSoni33@users.noreply.github.com>
2024-04-07 22:02:24 +0530

    Create xyz.java
    Create xyz.java

---
+++ Java_Code/xyz.java
@@ -0,0 +1,9 @@
+class HelloWorldxyz {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, Worlghjgjggd!"); 
+         System.out.println("Hello, vikash"); 
+         System.out.println("Hello, vikash2"); 
+         System.out.println("Hello, vikash3"); 
+    }
+} 
